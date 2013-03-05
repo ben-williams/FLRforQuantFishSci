ffc38300f91df1334d98b3c613963faed79247e5
iagomosqueira <iago.mosqueira@gmail.com>
2013-03-05 08:58:17 -0800

    Progressing

---
+++ Template/template.R
@@ -0,0 +1,12 @@
+# template.R - DESC
+# template.R
+
+# Copyright 2013 JRC FISHREG. Distributed under the GPL 2 or later
+# Maintainer: JRC FISHREG
+# $Id: $
+# Created:
+# Modified:
+
+# XX {{{
+# }}}
+
